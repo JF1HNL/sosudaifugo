c86bc917221184814a240d570a754329e006ce4a
JF1HNL <kimossan88@gmail.com>
2020-03-02 22:47:33 +0900

    ゲームスタートまで作った

--- App.js
+++ App.js
@@ -9,12 +9,61 @@ const ROLE_NAME_ID = {
   kankyaku : '682937592081416218'
 }
 
+// 重要なフラグ
+let game_flag = false;
+
+// トランプ生成してるところ
+class Card {
+  constructor(mark, num){
+    this.mark = mark;
+    this.num = num;
+    switch (num){
+      case 1:
+        this.char = 'A'
+        break;
+      case 10:
+        this.char = 'T'
+        break;
+      case 11:
+        this.char = 'J'
+        break;
+      case 12:
+        this.char = 'Q'
+        break;
+      case 13:
+        this.char = 'K'
+        break;
+      default:
+        this.char = String(num)
+    }
+    this.use = false
+  }
+}
+
+var cards = [];
+function init(){ //トランプのカードを作成
+  let x = 0;
+  for (var i = 1; i <= 13; i++){
+    cards[x] = new Card("♠", i);
+    x++;
+    cards[x] = new Card("☘", i);
+    x++;
+    cards[x] = new Card("❤", i);
+    x++;
+    cards[x] = new Card("♦", i);
+    x++;
+  };
+};init();
+
 client.on("ready", ()=> {
   console.log("ready...");
   client_msg_push('bot_control', 'server-start!')
 });
 
 client.on("message",message => {
+  if(message.author.bot && message.content === 'server-start!'){
+    role_reset(message);
+  }
   if(message.author.bot){
     return true
   }
@@ -45,7 +94,7 @@ client.login(token);
 // function
 function message_reply(message, text) {
   message.reply(text)
-  .then(message => console.log(`Sent message: ${text}`))
+  .then(message => console.log(`Reply message: ${text}`))
   .catch(console.error);
 }
 
@@ -57,8 +106,8 @@ function client_msg_push(channel_name, text) {
 }
 
 function role_reset(message) {
-  message_reply(message, '役職をリセットしました')
-  sosudaihugo_obj = new SOSUDAIHUGO_CLASS()
+  client_msg_push('bot_control', '役職をリセットしました')
+  sosudaihugo_obj = new SOSUDAIHUGO_CLASS(0, [], 0, [])
   message.guild.members.forEach(member => 
     member.setRoles([ROLE_NAME_ID['kankyaku']])
     .then(console.log(`Swich role: kankyaku`))
@@ -76,10 +125,7 @@ function role(message) {
   // message.member.addRole(ROLE_NAME_ID[msg])
   //   .then(console.log(`swich role: ${msg}`))
   //   .catch(console.error);
-  if(msg === 'player1' || msg === 'player2'){
-    sosudaihugo_obj[msg] = new PLAYER_CLASS(message.author.username, [])
-    console.log(sosudaihugo_obj)
-  }
+  sosudaihugo_obj[msg] = new PLAYER_CLASS(message.author.id, [])
   message_reply(message, `役職を${msg}に変更しました`)
   message.member.setRoles([ROLE_NAME_ID[msg]])
   .then(console.log(`Swich role: ${msg}`))
@@ -114,8 +160,10 @@ class SOSUDAIHUGO_CLASS {
   }
 }
 
-let sosudaihugo_obj = new SOSUDAIHUGO_CLASS()
+let sosudaihugo_obj = new SOSUDAIHUGO_CLASS(0, [], 0, [])
 
+
+// チャンネルごとの挙動
 function func_player1(message) {
   message_reply(message, 'player1') 
 }
@@ -125,8 +173,71 @@ function func_player2(message) {
 }
 
 function func_bot_control(message) {
-  message_reply(message, 'bot_control')
+  if(game_flag){
+    return;
+  }
+  if(message.content === 'new game'){
+    role_reset(message);
+    return;
+  }
+  if(sosudaihugo_obj.player1.user !== message.author.id && sosudaihugo_obj.player2.user !== message.author.id){
+    if(message.content === 'player1' && sosudaihugo_obj.player1.user === 0){
+      role(message);
+      if(sosudaihugo_obj.player1.user !== 0 && sosudaihugo_obj.player2.user !== 0){
+        game_start()
+      }
+      return;
+    }
+    if(message.content === 'player2' && sosudaihugo_obj.player2.user === 0){
+      role(message);
+      if(sosudaihugo_obj.player1.user !== 0 && sosudaihugo_obj.player2.user !== 0){
+        game_start()
+      }
+      return;
+    }
+  }
+  message_reply(message, message.content)
 }
+
+function game_start(params) {
+  console.log('game start!')
+  function all_chanel_msg_push(e){
+    client_msg_push('プレイヤー1', e);
+    client_msg_push('プレイヤー2', e);
+    client_msg_push('闘技場', e);
+    client_msg_push('bot_control', e);
+  }
+
+  game_flag = true;
+  const start_text = 
+  `\`\`\`
+  ********************
+  素数大富豪スタート！
+  ********************
+  \`\`\`
+  まずは各プレイヤーに手札を配布します。`;
+  all_chanel_msg_push(start_text);
+  for(let i = 0; i < 5; i++){
+    hand_add(sosudaihugo_obj.player1)
+    hand_add(sosudaihugo_obj.player2)
+  }
+  console.log(sosudaihugo_obj.player1.hand)
+  console.log(sosudaihugo_obj.player2.hand)
+}
+
+function hand_add(player){
+  while(1){
+    const random = Math.floor(Math.random() * cards.length)
+    const geted_card = cards[random]
+    if(geted_card.use){
+      continue
+    }
+    geted_card.use = true;
+    player.hand.push(geted_card)
+    break;
+  }
+}
+
 
 const ui_test = 
 `
